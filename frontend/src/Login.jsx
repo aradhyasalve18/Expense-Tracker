0dbb0030b0d0b33a9e15ba467b424e6141246502
Aradhya Salve <aradhyasalve18@gmail.com>
2026-05-18 22:16:47 +0530

    Production Backend URL

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -13,7 +13,7 @@ export default function Login({ onLogin, onGoSignup }) {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("http:// https://expense-tracker-6nhi.onrender.com/api/auth/login", {
         method:  "POST",
         headers: { "Content-Type": "application/json" },
         body:    JSON.stringify({ email: form.email, password: form.password }),
